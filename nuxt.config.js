89ba370cb5603fa804bd9112d11a6bd180f1627f
Shell Raiser <ksshailesh05@gmail.com>
2022-09-03 20:11:57 +0530

    dummy

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,23 @@ export default {
 		// https://go.nuxtjs.dev/vuetify
 		'@nuxtjs/vuetify',
 	],
-
+	vuetify: {
+		customVariables: ['~/assets/variables.scss'],
+		theme: {
+			dark: true,
+			themes: {
+				dark: {
+					primary: colors.blue.darken2,
+					accent: colors.grey.darken3,
+					secondary: colors.amber.darken3,
+					info: colors.teal.lighten1,
+					warning: colors.amber.base,
+					error: colors.deepOrange.accent4,
+					success: colors.green.accent3,
+				},
+			},
+		},
+	},
 	// Modules: https://go.nuxtjs.dev/config-modules
 	modules: [
 		// https://go.nuxtjs.dev/pwa
@@ -79,23 +95,7 @@ export default {
 	},
 
 	// Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
-	vuetify: {
-		customVariables: ['~/assets/variables.scss'],
-		theme: {
-			dark: true,
-			themes: {
-				dark: {
-					primary: colors.blue.darken2,
-					accent: colors.grey.darken3,
-					secondary: colors.amber.darken3,
-					info: colors.teal.lighten1,
-					warning: colors.amber.base,
-					error: colors.deepOrange.accent4,
-					success: colors.green.accent3,
-				},
-			},
-		},
-	},
+	
 
 	// Build Configuration: https://go.nuxtjs.dev/config-build
 	build: {},
