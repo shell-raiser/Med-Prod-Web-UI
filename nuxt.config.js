cf32e567858f0987f9032cfd9959b3e2dd117282
Shell Raiser <ksshailesh05@gmail.com>
2022-09-07 06:28:55 +0530

    update icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
 			{ hid: 'description', name: 'description', content: '' },
 			{ name: 'format-detection', content: 'telephone=no' },
 		],
-		link: [{ rel: 'icon', type: 'image/x-icon', href: '/logo.jpeg' }],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.png' }],
 	},
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
