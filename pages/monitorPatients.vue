63f6b8ba7a588ead401df598c1a4542d0308828b
shell-raiser <ksshailesh05@gmail.com>
2022-09-21 11:18:00 +0000

    import data into DOM

--- pages/monitorPatients.vue
+++ pages/monitorPatients.vue
@@ -1,7 +1,53 @@
 <template>
     <v-container>
         <h1>All Patients</h1>
-        <personCard />
+        <p>{{allPatients}}</p>
+        <v-container style="padding:20px;">
+            <v-card>
+                <v-row class=" justify-space-around ">
+                    <span>BP</span>
+                    <span>TEMP</span>
+                    <span>ECG</span>
+                    <span>SPO2</span>
+                    <span>Time stamp</span>
+                </v-row>
+            </v-card>
+        </v-container>
+        <!-- <personCard /> -->
     </v-container>
-
 </template>
+<script>
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue} from "firebase/database";
+
+var config = {
+    apiKey: "AIzaSyB9ahJNsL4sfy4TVlc9jGymrkJPlbbJ0-w",
+    authDomain: "hospicare-app22.firebaseapp.com",
+    databaseURL: "https://hospicare-app22-default-rtdb.firebaseio.com"
+};
+var secondaryApp = initializeApp(config, "Secondary");
+
+
+export default ({
+    data() {
+        return {
+            allPatients:{}
+        }
+    },
+    mounted() {
+        try {
+            const db = getDatabase();
+            const starCountRef =  ref(db, this.$fire.auth.currentUser.displayName+'/logs')
+            onValue(starCountRef, (snapshot) => {
+                const data = snapshot.val();
+                this.allPatients = data
+                console.log(allPatients)
+            });
+        }
+        catch (e) {
+            console.log(e)
+        }
+    },
+    
+    })
+</script>
--- pages/patientLogs.vue
+++ pages/patientLogs.vue
@@ -1,8 +1,55 @@
 <template>
     <v-container>
         <h1>NAME HERE</h1>
-        <patientLog />
-        <patientLog />
-        <patientLog />
+        <v-container style="padding:20px;">
+            <v-card>
+                <v-row class="justify-space-around">
+                    <span>BP</span>
+                    <span>TEMP</span>
+                    <span>ECG</span>
+                    <span>SPO2</span>
+                    <span>Time stamp</span>
+                </v-row>
+            </v-card>
+        </v-container>
     </v-container>
 </template>
+<script>
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue} from "firebase/database";
+
+var config = {
+    apiKey: "AIzaSyB9ahJNsL4sfy4TVlc9jGymrkJPlbbJ0-w",
+    authDomain: "hospicare-app22.firebaseapp.com",
+    databaseURL: "https://hospicare-app22-default-rtdb.firebaseio.com"
+};
+var secondaryApp = initializeApp(config, "Secondary");
+
+
+export default ({
+    mounted() {
+        try {
+
+
+            const db = getDatabase();
+// const starCountRef = ref(db, 'posts/' + postId + '/starCount');
+
+            const starCountRef =  ref(db, this.$fire.auth.currentUser.displayName+'/logs')
+            onValue(starCountRef, (snapshot) => {
+                const data = snapshot.val();
+                console.log(data)
+            });
+        }
+        catch (e) {
+            console.log(e)
+        }
+    },
+
+    methods: {
+        consoleIt() {
+            // console.log("clicked");
+
+        }
+    },
+})
+</script>
